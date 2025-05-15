8049f9a3a15068dd956d31719f57deb1a5330dda
Author <example@mail.ru>
2025-05-15 16:13:19 +0300

    Добавили в генератор верхний регистр(2)

--- password_generator.cs
+++ password_generator.cs
@@ -13,7 +13,7 @@ class Program
 
     static string GeneratePassword(int length)
     {
-        const string validChars = "abcdefghijklmnopqrstuvwxyz";
+        const string validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
         Random random = new Random();
         char[] passwordChars = new char[length];
 
